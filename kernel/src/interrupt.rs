796ef49f214ee3c58aa15028c1b22af93dcc27a0
Totsugekitai <37617413+Totsugekitai@users.noreply.github.com>
2022-06-21 20:55:44 +0900

    [update]UARTでの文字入力に対応

---
+++ kernel/src/interrupt.rs
@@ -0,0 +1,40 @@
+use crate::uart;
+use lazy_static::lazy_static;
+use x86_64::structures::idt::{self, InterruptDescriptorTable, InterruptStackFrame};
+
+use crate::println;
+
+lazy_static! {
+    static ref IDT: InterruptDescriptorTable = unsafe {
+        let mut idt = InterruptDescriptorTable::new();
+        idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.double_fault
+            .set_handler_fn(double_fault_handler)
+            .set_stack_index(0);
+
+        idt[36].set_handler_fn(uart::uart_handler);
+
+        idt
+    };
+}
+
+pub fn init() {
+    IDT.load()
+}
+
+extern "x86-interrupt" fn breakpoint_handler(stack_frame: InterruptStackFrame) {
+    println!("EXCEPTION: BREAKPOINT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn double_fault_handler(
+    stack_frame: InterruptStackFrame,
+    _error_code: u64,
+) -> ! {
+    panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+pub fn notify_end_of_interrupt() {
+    unsafe {
+        core::ptr::write_volatile(0xFEE0_00B0 as *mut u32, 0);
+    }
+}
--- kernel/src/uart.rs
+++ kernel/src/uart.rs
@@ -1,13 +1,23 @@
+use crate::{interrupt, ioapic, print};
 use alloc::sync::Arc;
 use core::fmt::Write;
 use lazy_static::lazy_static;
 use spin::Mutex;
-use x86_64::instructions::{
-    interrupts::without_interrupts,
-    port::{PortReadOnly, PortWriteOnly},
+use x86_64::{
+    instructions::{
+        interrupts::without_interrupts,
+        port::{PortReadOnly, PortWriteOnly},
+    },
+    structures::idt::InterruptStackFrame,
 };
 
-const COM1: u16 = 0x3f8;
+pub const COM1: u16 = 0x3f8;
+pub const COM2: u16 = 0x2f8;
+pub const COM3: u16 = 0x3e8;
+pub const COM4: u16 = 0x2e8;
+
+const IRQ_COM1: u32 = 4;
+const IRQ_COM2: u32 = 3;
 
 lazy_static! {
     pub static ref UART: Arc<Mutex<Uart>> = Arc::new(Mutex::new(Uart { com: COM1 }));
@@ -38,6 +48,8 @@ impl Uart {
 
             PortReadOnly::<u16>::new(self.com + 2).read();
             PortReadOnly::<u16>::new(self.com).read();
+
+            ioapic::enable(IRQ_COM1, 0);
         });
     }
 
@@ -84,4 +96,18 @@ pub fn init() {
     unsafe {
         UART.lock().init();
     }
+}
+
+pub extern "x86-interrupt" fn uart_handler(_: InterruptStackFrame) {
+    let mut c = b'\0';
+    without_interrupts(|| unsafe {
+        c = UART.lock().read();
+        interrupt::notify_end_of_interrupt();
+        print!("{}", c as char);
+    });
+}
+
+pub fn remove_screen() {
+    let mut uart = UART.lock();
+    let _ = uart.write_str("\x1b[2J\x1b[1;1H");
 }
